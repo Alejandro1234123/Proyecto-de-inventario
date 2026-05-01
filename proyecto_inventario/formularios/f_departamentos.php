b1e657d3ecffedbe2dfad2fb990bee48189cbd50
Alejandro1234123 <alejandro2009206goku@gmail.com>
2026-05-01 12:16:16 -0400

    Delete proyecto_inventario directory

--- proyecto_inventario/formularios/f_departamentos.php
+++
@@ -1,8 +0,0 @@
-<form action="../guardar.php" method="POST">
-    <input type="hidden" name="tabla" value="departamentos">
-    <label>Nombre Departamento:</label>
-    <input type="text" name="nombre_departamento" class="form-control" required>
-    <label>Responsable:</label>
-    <input type="text" name="responsable" class="form-control">
-    <button type="submit" class="btn btn-primary mt-3">Guardar</button>
-</form>
